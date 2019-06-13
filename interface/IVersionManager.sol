10e835157129275fa261513a65eb28599a8bc316
kaikai1024 <kaikaifeng1024@gmail.com>
2019-06-13 17:57:27 +0800

    Use protocol version.

--- interface/IVersionManager.sol
+++ interface/IVersionManager.sol
@@ -3,5 +3,10 @@ pragma solidity ^0.4.24;
 /// @title The interface of version manager
 /// @author ["Cryptape Technologies <contact@cryptape.com>"]
 interface IVersionManager {
+    /// @notice Deprecated. Check the setProtocolVersion
     function setVersion(uint32 _version) external;
+    function setProtocolVersion(uint32 _version) external;
+    /// @notice Deprecated. Check the setProtocolVersion
+    function getVerison() external returns (uint32);
+    function getProtocolVerison() external returns (uint32);
 }
