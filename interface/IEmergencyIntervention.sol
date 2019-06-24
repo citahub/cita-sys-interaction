1cfbff5904640aa58f61cd5cb13850f1f9528319
kaikai1024 <kaikaifeng1024@gmail.com>
2019-06-24 10:34:08 +0800

    Rename brake to intervention.

--- interface/IEmergencyIntervention.sol
+++ interface/IEmergencyIntervention.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.4.24;
 
-/// @title The interface of emergency brake
+/// @title The interface of emergency intervention
 /// @author ["Cryptape Technologies <contact@cryptape.com>"]
-interface IEmergencyBrake {
+interface IEmergencyIntervention {
     function setState(bool _state) external;
 }
